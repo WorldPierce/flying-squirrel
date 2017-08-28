b51678443a60cdc462802a6c14f79af27645d116
worldpierce <bildo316@gmail.com>
2017-08-28 10:34:30 -0400

    collision, music added

--- obstical.js
+++ obstical.js
@@ -2,17 +2,27 @@ function Obstical(maxHoleSize, spd) {
 	// this.top = random(window.innerHeight/2);
 	// this.bottom = random(window.innerHeight/2);
 	// this. middle = random(window.innerHeight);
-	this.x = window.innerWidth;
-	this.w = window.innerWidth * .06;
+	this.x = width;
+	this.w = window.innerWidth * .04;
 	//space is space cover with innerheight being the entire space
 	//space = 510 is smallest we want
-	this.space = 505 - maxHoleSize;
+	this.space = height - maxHoleSize;
 	// this.space = (random() * (window.innerHeight - minHoleSize)) + maxHoleSize;
 	//this.space = (random() * (window.innerHeight) + (maxHoleSize - minHoleSize);
 	this.top = random() * (this.space);
 	this.bottom = this.space - this.top;
-	this.speed = 5.0 + spd;
+	this.speed = 6.0 + spd;
 	this.img = loadImage("images/forest1860.png");
+	
+
+	this.hits = function(squirrel) {
+		if(squirrel.y + 10 < this.top || squirrel.y > height - this.bottom) {
+			if(squirrel.x > this.x && squirrel.x < this.x + this.w) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	
 
@@ -21,9 +31,9 @@ function Obstical(maxHoleSize, spd) {
 		//fill(255);
 		// rect(this.x, 0, this.w, this.top);
 		// rect(this.x, window.innerHeight - this.bottom, this.w, this.bottom);
-		//console.log(this.space);
+		//console.log(window.innerHeight);
 		image(this.img, this.x, 0, this.w, this.top);
-		image(this.img, this.x, window.innerHeight - this.bottom, this.w, this.bottom);
+		image(this.img, this.x, height - this.bottom, this.w, this.bottom);
 
 		//setTimeout(function(){ image(this.img, this.x, window.innerHeight + this.bottom, this.w, this.h); }, 1000);
 	}
--- sketch.js
+++ sketch.js
@@ -3,84 +3,152 @@ var squrriel;
 var obsticals = [];
 var flyingObsticals = [];
 var myRand = 15;
-// var song;
+var song;
 // var maxHoleSize = 0;
-// var minHoleSize = 100.0;
-
-var maxHoleSize = 200;
+var minHoleSize = 100.0;
+var logo;
+var maxHoleSize = 250;
 var spd = 0.0;
+var doDraw;
+var newGame = true;
 
-// function preload() {
-// 	song = loadSound("sounds/Gianni.mp3");
-// }
+
+function preload() {
+	song = loadSound("sounds/Gianni.mp3");
+}
 function setup() {
-	// song.play();
-	// song.setVolume(0.5);
-	squrriel = new Squrriel(); 
+	var button = createButton("reset");
+	button.mousePressed(resetSketch);
+	createCanvas(1000, 525);
+	song.play();
+	song.setVolume(0.5);
+	//song.addCue(0.10, setUpBg);
+	song.addCue(1.00, resetSketch);
 	bg = loadImage("images/background3.png");
-    createCanvas(window.innerWidth, window.innerHeight);
-    obsticals.push(new Obstical(maxHoleSize, spd));
+	logo = new Logo();
+
+
+	//resetSketch();
+}
+
+function setUpBg() {
+	bg = loadImage("images/background3.png");
+	logo = new Logo();
+}
+
+function resetSketch() {
+	frameCount = 0;
+	doDraw = true;
+	obsticals.length = 0;
+	flyingObsticals = [];
+	myRand = 15;
+	newGame = true;
+
+// var maxHoleSize = 0;
+	minHoleSize = 100.0;
+	//logo;
+	maxHoleSize = 250;
+	spd = 0.0;
+	
+	squrriel = new Squrriel(); 
+	
+    
+    obsticals.push(new Obstical((Math.random() * maxHoleSize) + minHoleSize, spd));
     flyingObsticals.push(new Obstical2(Math.floor((Math.random() * myRand)),spd));
 }
 
 function draw() {
 	background(bg);
-	squrriel.update();
-	squrriel.show();
+	logo.show();
+	//image(logo, width/2 - 500, height/2 - 170, 896, 176);
+	if(doDraw){
+		logo.update();
+		squrriel.update();
+		squrriel.show();
 
-	if(frameCount % 60 == 0) {
-		obsticals.push(new Obstical(maxHoleSize, spd));
-		flyingObsticals.push(new Obstical2(Math.floor((Math.random() * myRand)),spd));
-	}
-
-
-	for(var i = obsticals.length - 1 ; i >= 0; i--) {
-		// if(Math.floor((Math.random() * 2)) < 1) {
-		// 	obsticals[i].show();
-		// }
-		obsticals[i].show();
-		obsticals[i].update();
-		
-		if(obsticals[i].offScreen()) {
-			obsticals.splice(i, 1);
-			if(spd < 12.0) {
-				spd += 0.15;
-			}
-			if(maxHoleSize > 0) {
-				maxHoleSize -= 2;
-			}
+		if(!newGame && frameCount % 75 == 0) {
+			obsticals.push(new Obstical((Math.random() * maxHoleSize) + minHoleSize, spd));
+			flyingObsticals.push(new Obstical2(Math.floor((Math.random() * myRand)),spd));
 		}
-		if(flyingObsticals[i] != null) {
-			flyingObsticals[i].show1();
-			flyingObsticals[i].update();
-			if(flyingObsticals[i].offScreen()) {
-			flyingObsticals.splice(i,1);
-			if(myRand > 12) {
-				myRand -= 1;
+
+
+		for(var i = obsticals.length - 1 ; i >= 0; i--) {
+			// if(Math.floor((Math.random() * 2)) < 1) {
+			// 	obsticals[i].show();
+			// }
+			obsticals[i].show();
+			obsticals[i].update();
+
+			if(obsticals[i].hits(squrriel)){
+				//console.log("IHT");
+				doDraw = false;
+			}
+			
+			if(obsticals[i].offScreen()) {
+				obsticals.splice(i, 1);
+				if(spd < 12.0) {
+					spd += 0.15;
+				}
+				if(maxHoleSize > 0) {
+					maxHoleSize -= 2;
+				}
+				if(minHoleSize > 55) {
+					minHoleSize -= 2;
+				}
+			}
+			if(flyingObsticals[i] != null) {
+				flyingObsticals[i].show1();
+				flyingObsticals[i].update();
+				if(flyingObsticals[i].hits(squrriel)){
+					//console.log("IHT");
+					console.log("hit");
+					if(flyingObsticals[i].isNut) {
+						//flyingObsticals[i].clear();
+
+
+					} else {
+						doDraw = false;
+					}
+					
+				}
+				if(flyingObsticals[i].offScreen()) {
+				flyingObsticals.splice(i,1);
+				if(myRand > 12) {
+					myRand -= 1;
+				}
+				
+				}
 			}
 			
 		}
+		if(keyIsDown(32)) {
+			squrriel.glide();
 		}
-		
 	}
-	if(keyIsDown(32)) {
-		squrriel.glide();
-	}
+}
 
-
-
-  // if (mouseIsPressed) {
-  //   fill(0);
-  // } else {
-  //   fill(255);
-  // }
-  // ellipse(mouseX, mouseY, 80, 80);
+function play() {
+	doDraw = true;
+	//logo.hide();
 }
 
 function keyPressed() {
 	if(key == ' ') {
 		squrriel.up();
+		newGame = false;
 		//console.log("space");
+	}
+	if(key == 80) {
+		console.log('p');
+		if(song.isPlaying()){
+			song.pause();
+		}
+		else {
+			song.play();
+		}
+	}
+	if(key == 13 && doDraw == false) {
+		doDraw = true;
 	}
 }
 
